8644e6f9c368e067df8bd160d6efb60a2010d0b1
Derek Chan <ychan3@stuy.edu>
2018-10-20 19:29:34 -0400

    first draft

--- sieve.c
+++ sieve.c
@@ -1,11 +1,34 @@
-#import <stdio.h>
-#import <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int sieve(int end){
-	char arr[98] = {0};
+	int nums = (int)(end*log(end)*1.15);
+	char * arr = calloc(nums, sizeof(char));
+	int iter = 0;
 	int counter = 2;
-	int prime_num = end - 1;
+	int prime_num = end;
+	int current_check = 2;
 	while(prime_num){
-		
+		if(counter == current_check && !arr[iter]){
+			printf("prime %d: %d\n", prime_num, current_check);
+			prime_num--;
+			arr[iter] = 1;
+		}
+		else if(!arr[iter] && (counter)%current_check == 0){
+			arr[iter] = 1;
+		}
+		if(counter == nums){
+			counter = current_check;
+			current_check++;
+			iter = current_check - 3;
+		}
+		counter++;
+		iter++;
 	}
+	return current_check;
+}
+
+int main(){
+	printf("%d\n", sieve(1000000));
 }
