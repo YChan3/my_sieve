e914d311adac6ba0c7a2b9b80cf67a237364b98a
Derek Chan <ychan3@stuy.edu>
2018-10-21 01:54:06 -0400

    added stuff

--- sieve.c
+++ sieve.c
@@ -1,41 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 int sieve(int end){
 	int nums = (int)(end*log(end)*1.15);
-	char arr[nums];
-	int i = nums;
-	while(i+1){
-		arr[i] = 0;
-		i--;
-	}
+	char * arr = calloc(1, nums);
 
-	int iter = 0;
 	int counter = 2;
-	int prime_num = end;
 	int current_check = 2;
-	while(prime_num){
-		if(counter == current_check && !arr[iter]){
-			printf("prime %d: %d\n", prime_num, current_check);
-			prime_num--;
-			arr[iter] = 1;
-		}
-		else if(!arr[iter] && (counter)%current_check == 0){
-			arr[iter] = 1;
-		}
-		if(counter == nums){
+
+	while(end){
+		if(counter == current_check){
+			//printf("prime %d: %d\n", end, current_check);
+			if(!--end){
+				return current_check;
+			}
+
+			int c2 = counter;
+			while(c2 < nums){
+				//printf("c2 %d\n", c2);
+				arr[c2] = 1;
+				c2 += counter;
+			}
+
+			for(int i = current_check; i < nums; i++){
+				//printf("i %d\n", i);
+				if(arr[i] == 0){
+					current_check = i;
+					break;
+				}
+			}
 			counter = current_check;
-			current_check++;
-			iter = current_check - 3;
 		}
-		counter++;
-		iter++;
 	}
 	return current_check;
 }
 
+int Sieve(int n){
+  int s=(int)(n*log(n)*1.5);
+  int c=0;
+  char *a = malloc(s);
+  for(int i=2; i <s; i++){
+    if(a[i]!='h'){
+      c++;
+      for(int x=i*2; x <s; x+=i){
+	a[x]='h';
+      }
+    }
+    if(c==n){
+      return i;
+    }
+  }
+  return 0;
+}
+
 int main(){
-	printf("hi");
-	printf("%d\n", sieve(1000000));
+	// printf("%d\n", sieve(1000000));
+	printf("%d\n", Sieve(1000000));
 }
